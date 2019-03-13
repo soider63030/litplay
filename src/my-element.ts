bbbc4cb7ec5ba9e5869b71b637d3938e6662d581
soider63030 <48270607+soider63030@users.noreply.github.com>
2019-03-13 15:43:29 +0800

    work
    work

--- src/my-element.ts
+++ src/my-element.ts
@@ -3,10 +3,25 @@ import { LitElement, html, customElement, property } from 'lit-element';
 @customElement('my-element')
 export class MyElement extends LitElement {
     @property()
-    foo = 'foo';
+    foo = {"firstName":"BEN",
+    "lastName":"TAN",
+    "username":"SO HANDSOME",
+    "email":"Gladyce.Weber@yahoo.com"};
 
     render() {
-        return html`<p>${this.foo}</p>`;
+        return html`
+        <style>
+        *{margin:10px; padding:10px;}
+        #h11 {color:blue;font-size:100px;shadow: 2px 4px 3px rgba(0,0,0,0.3); }
+        #h22 {color:green; font-size:100px;}
+        #p1{color:red; font-size:100px;}   
+       
+        </style>
+        <h1 id=h11>${this.foo.firstName}</h1>
+        <h2 id=h22>${this.foo.lastName}</h2>
+        <p id =p1>${this.foo.username}</p>`;
+
+
     }
 
     firstUpdated(changedProperties:any) {
@@ -15,7 +30,8 @@ export class MyElement extends LitElement {
         });
         fetch('api/user')
             .then((response) => response.json())
-            .then((bodyRes) => console.log(bodyRes));
-
+            .then((bodyRes) =>{
+                this.foo = bodyRes;
+            });
+        }   
     }
-}
